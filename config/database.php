66cdbbd0483ee7c142efb4c059b8e499c2b3ed2e
Jon Norton <jmichaeln5@gmail.com>
2018-05-21 19:58:40 -0400

    Database setup

--- config/database.php
+++ config/database.php
@@ -1,9 +1,8 @@
 <?php
 
-$url = parse_url(getenv("DATABASE_URL"));
-
+$url = parse_url(getenv('DATABASE_URL'));
 $host = $url["host"]??null;
-$user = $url["user"]??null;
+$username = $url["user"]??null;
 $password = $url["pass"]??null;
 $database = substr($url["path"], 1)??null;
 
@@ -76,16 +75,15 @@ return [
 
         'pgsql_production' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
